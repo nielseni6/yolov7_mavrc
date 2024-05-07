576c170e393b1be1e1f59788c5c91ef0450919ee
nielseni6 <nielseni6@students.rowan.edu>
2024-05-07 15:41:04 -0400

    removed pgt-lr-decay-add since pgt_coeff at 0 causing memory leak bug and crashes

--- train_pgt.py
+++ train_pgt.py
@@ -67,8 +67,8 @@ def train(hyp, opt, device, tb_writer=None):
     for k_epoch in range(opt.epochs):
         if ((k_epoch % opt.pgt_lr_decay_step) == 0) and (k_epoch != 0):
             pgt_coeff *= opt.pgt_lr_decay
-            if pgt_coeff == 0.0:
-                pgt_coeff += opt.pgt_lr_decay_add
+            # if pgt_coeff == 0.0:
+            #     pgt_coeff += opt.pgt_lr_decay_add
         pgt_coeff_list.append(pgt_coeff)
         # print(f'PGT learning rate decayed to {opt.pgt_coeff} at epoch {epoch}')
     #################################################################################
@@ -715,10 +715,10 @@ if __name__ == '__main__':
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3') 
     ############################## PGT Variables ############################### 
     parser.add_argument('--seed', type=int, default=None, help='reproduce results') 
-    parser.add_argument('--pgt-coeff', type=float, default=0.0, help='learning rate for plausibility gradient') 
-    parser.add_argument('--pgt-lr-decay', type=float, default=1.0, help='learning rate decay for plausibility gradient') 
+    parser.add_argument('--pgt-coeff', type=float, default=0.001, help='learning rate for plausibility gradient') 
+    parser.add_argument('--pgt-lr-decay', type=float, default=160.0, help='learning rate decay for plausibility gradient') 
     parser.add_argument('--pgt-lr-decay-step', type=int, default=250.0, help='learning rate decay step for plausibility gradient') 
-    parser.add_argument('--pgt-lr-decay-add', type=float, default=0.3, help='learning rate decay to add to pgt coeff')
+    # parser.add_argument('--pgt-lr-decay-add', type=float, default=0.3, help='learning rate decay to add to pgt coeff')
     parser.add_argument('--n-max-attr-labels', type=int, default=100, help='maximum number of attribution maps generated for each image') 
     parser.add_argument('--out_num_attrs', nargs='+', type=int, default=[2,], help='Output for generating attribution maps (for loss_attr 0: box, 1: obj, 2: cls)') 
     parser.add_argument('--clean_plaus_eval', action='store_true', help='If true, calculate plausibility on clean, non-augmented images and labels') 
@@ -729,7 +729,7 @@ if __name__ == '__main__':
     parser.add_argument('--plaus_results', action='store_true', help='If true, calculate plausibility on clean, non-augmented images and labels during testing') 
     ################################### PGT Loss Variables ################################### 
     parser.add_argument('--dist_reg_only', type=bool, default=True, help='If true, only calculate distance regularization and not plausibility') 
-    parser.add_argument('--focus_coeff', type=float, default=0.25, help='focus_coeff') 
+    parser.add_argument('--focus_coeff', type=float, default=0.18, help='focus_coeff') 
     parser.add_argument('--iou_coeff', type=float, default=0.075, help='iou_coeff') 
     parser.add_argument('--dist_coeff', type=float, default=1.0, help='dist_coeff') 
     parser.add_argument('--bbox_coeff', type=float, default=0.0, help='bbox_coeff') 
